4be522cd20d0b3d2e1cc68dd2a397a270845d6f3
Hayden <haydenshore@gmail.com>
2022-05-12 02:02:17 +0100

    Small tweak to make deployment work. Disables HttpsRedirection and Enables ForwardHeadersMiddleware for deployment behind a  reverse proxy. Adds a KnownProxie IP

--- MVC_Sort_Test/Program.cs
+++ MVC_Sort_Test/Program.cs
@@ -1,6 +1,9 @@
+using Microsoft.AspNetCore.HttpOverrides;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using MVC_Sort_Test.Data;
+using System.Net;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddDbContext<MVC_Sort_TestContext>(options =>
@@ -8,18 +11,34 @@ builder.Services.AddDbContext<MVC_Sort_TestContext>(options =>
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+builder.Services.Configure<ForwardedHeadersOptions>(options =>
+{
+    options.KnownProxies.Add(IPAddress.Parse("10.0.0.10"));
+});
+
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
-{
+{   
+    //IF NOT DEVELOPMENT
     app.UseExceptionHandler("/Home/Error");
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
+    app.UseForwardedHeaders(new ForwardedHeadersOptions
+    {
+        ForwardedHeaders = ForwardedHeaders.XForwardedFor | ForwardedHeaders.XForwardedProto
+    });
+}
+else
+{
+    //IF IN DEVELOPMENT
+    app.UseHttpsRedirection();
+
 }
 
-app.UseHttpsRedirection();
+
 app.UseStaticFiles();
 
 app.UseRouting();
